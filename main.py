0925976308c482589ab490367be8d61f5e9c321b
Lucius Holzinger <luciholzinger@gmail.com>
2025-11-15 08:38:57 -0800

    the ability to download the 2024 stats is implemented

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import pandas as pd
 import kaggle
 import matplotlib.pyplot as plt
 
-kaggle.api.authenticate()
-DOWNLOAD_ROOT = "drgilermo/nba-players-stats"
+SEASON_STATS_DOWNLOAD_ROOT = "drgilermo/nba-players-stats"
+TWENTY_FOUR_DOWNLOAD_ROOT = "eduardopalmieri/nba-player-stats-season-2425"
 HOUSING_PATH = "datasets/nba"
+OTHER_HOUSING = "datasets/nba/2024"
 
 
-def fetch_nba_data(housing_url=DOWNLOAD_ROOT, housing_path=HOUSING_PATH):
+def fetch_nba_data(housing_url=SEASON_STATS_DOWNLOAD_ROOT, housing_path=HOUSING_PATH):
+    kaggle.api.authenticate()
     if not os.path.isdir(housing_path):
         kaggle.api.dataset_download_files(housing_url, path=housing_path, unzip=True)
 
@@ -18,13 +20,20 @@ def load_season_stats_data(housing_path=HOUSING_PATH):
     return pd.read_csv(csv_path)
 
 
+def fetch_twenty_twenty_four_nba_data(housing_url=TWENTY_FOUR_DOWNLOAD_ROOT, housing_path=OTHER_HOUSING):
+    kaggle.api.authenticate()
+    if not os.path.isdir(housing_path):
+        kaggle.api.dataset_download_files(housing_url, path=housing_path, unzip=True)
+
+
 def main():
     fetch_nba_data()
+    fetch_twenty_twenty_four_nba_data()
     seasons_stats = load_season_stats_data()
     # seasons_stats_head = seasons_stats.head()
     print("obama")
-    seasons_stats.hist(bins=50, figsize=(20, 15))
-    plt.show()
+    # seasons_stats.hist(bins=50, figsize=(20, 15))
+    # plt.show()
 
 
 if __name__ == '__main__':
