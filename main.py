986bdddba9f7da1bf91a928b3545618d30d0cc03
Lucius Holzinger <luciholzinger@gmail.com>
2025-11-15 08:43:31 -0800

    the new dataset has been loaded in. ready to visualize

--- main.py
+++ main.py
@@ -26,14 +26,21 @@ def fetch_twenty_twenty_four_nba_data(housing_url=TWENTY_FOUR_DOWNLOAD_ROOT, hou
         kaggle.api.dataset_download_files(housing_url, path=housing_path, unzip=True)
 
 
+def load_twenty_four_stats_data(housing_path=OTHER_HOUSING):
+    csv_path = os.path.join(housing_path, "database_24_25.csv")
+    return pd.read_csv(csv_path)
+
+
 def main():
     fetch_nba_data()
     fetch_twenty_twenty_four_nba_data()
     seasons_stats = load_season_stats_data()
+    twenty_four_season_stats = load_twenty_four_stats_data()
     # seasons_stats_head = seasons_stats.head()
+    # twenty_four_head = twenty_four_season_stats.head()
     print("obama")
-    # seasons_stats.hist(bins=50, figsize=(20, 15))
-    # plt.show()
+    seasons_stats.hist(bins=50, figsize=(20, 15))
+    plt.show()
 
 
 if __name__ == '__main__':
